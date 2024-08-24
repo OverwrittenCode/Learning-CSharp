7943da8453085e243ce287c359dd47ab5437ca02
OverwrittenCode <kylethapar2007@outlook.com>
2024-08-24 20:47:56 +0100

    refactor(ConsoleUtils): add new utils method

--- Learning/Utils/ConsoleUtils.cs
+++ Learning/Utils/ConsoleUtils.cs
@@ -12,4 +12,47 @@ internal static class ConsoleUtils
 
         Console.ForegroundColor = originalColour;
     }
+
+    public static T GetEnumChoice<T>(T[] values)
+        where T : struct, Enum
+    {
+        HighlightConsoleLine("[TURN]: Pick your option from the list:", ConsoleColor.Magenta);
+
+        Console.WriteLine();
+
+        var originalColour = Console.ForegroundColor;
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+        foreach (T value in values)
+        {
+            Console.WriteLine($"{Convert.ToInt32(value)} - {value}");
+        }
+
+        Console.ForegroundColor = originalColour;
+
+        Console.WriteLine();
+
+        HighlightConsoleLine("Your Choice", ConsoleColor.Magenta);
+
+        int choice;
+
+        while (!int.TryParse(Console.ReadLine(), out choice) || !Enum.IsDefined(typeof(T), choice))
+        {
+            HighlightConsoleLine(
+                "[ERROR]: Invalid choice. Please enter a number corresponding to an option.",
+                ConsoleColor.Red
+            );
+        }
+
+        return (T)(object)choice;
+    }
+
+    public static T GetEnumChoice<T>()
+        where T : struct, Enum
+    {
+        var values = Enum.GetValues<T>();
+
+        return GetEnumChoice(values);
+    }
 }
