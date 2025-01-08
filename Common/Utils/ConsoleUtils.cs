9d354a68ae9dcb19cd3bc9336af1c8c867682587
OverwrittenCode <kylethapar2007@outlook.com>
2025-01-08 15:38:16 +0000

    refactor: improve ConsoleUtils class

--- Common/Utils/ConsoleUtils.cs
+++ Common/Utils/ConsoleUtils.cs
@@ -2,55 +2,26 @@ namespace Common.Utils;
 
 public static class ConsoleUtils
 {
-    private static void SendMessage(string message, Action<string>? provider = null)
-    {
-        if (provider is null)
-        {
-            Console.WriteLine(message);
-        }
-        else
-        {
-            provider(message);
-        }
-    }
-
-    public static void GenerateText(string message)
-    {
-        for (var i = 0; i < message.Length; i++)
-        {
-            var letter = message[i];
-
-            if (i == message.Length - 1)
-            {
-                Console.WriteLine(letter);
-            }
-            else
-            {
-                Console.Write(letter);
-            }
-
-            Thread.Sleep(20);
-        }
-    }
-
-    public static void HighlightConsoleLine(string message, ConsoleColor colour, Action<string>? provider = null)
+    public static void HighlightConsoleLine(string message, ConsoleColor colour, Action<string> provider)
     {
         ConsoleColor originalColour = Console.ForegroundColor;
-
         Console.ForegroundColor = colour;
+        provider(message);
+        Console.ForegroundColor = originalColour;
+    }
 
-        SendMessage(message, provider);
-
+    public static void HighlightConsoleLine(string message, ConsoleColor colour)
+    {
+        ConsoleColor originalColour = Console.ForegroundColor;
+        Console.ForegroundColor = colour;
+        Console.WriteLine(message);
         Console.ForegroundColor = originalColour;
     }
 
     public static bool GetBooleanChoice(string messageBody)
     {
-        var message = $"{messageBody}? (y/n)";
-
-        HighlightConsoleLine(message, ConsoleColor.Magenta);
-
-        return Console.ReadLine()?.Trim().ToLower() == "y";
+        HighlightConsoleLine($"{messageBody}? (y/n)", ConsoleColor.Magenta);
+        return Console.ReadLine()?.Trim().ToLower() is "y" or "yes" or "t" or "true" or "1";
     }
 
     public static T GetEnumChoice<T>(T[] values, string messageCategory = "") where T : struct, Enum
@@ -60,14 +31,9 @@ public static class ConsoleUtils
             messageCategory = $"[{messageCategory.ToUpper()}]: ";
         }
 
-        var message = $"{messageCategory}Pick your option from the list:";
-
-        HighlightConsoleLine(message, ConsoleColor.Magenta);
-
+        HighlightConsoleLine($"{messageCategory}Pick your option from the list:", ConsoleColor.Magenta);
         Console.WriteLine();
-
         ConsoleColor originalColour = Console.ForegroundColor;
-
         Console.ForegroundColor = ConsoleColor.DarkYellow;
 
         foreach (T value in values)
@@ -76,25 +42,18 @@ public static class ConsoleUtils
         }
 
         Console.ForegroundColor = originalColour;
-
         Console.WriteLine();
-
         HighlightConsoleLine("Your Choice", ConsoleColor.Magenta);
 
-        int choice;
+        T choice;
 
-        while (!Int32.TryParse(Console.ReadLine(), out choice) || !Enum.IsDefined(typeof(T), choice))
+        while (!Enum.TryParse(Console.ReadLine()?.Trim(), true, out choice) || !Enum.IsDefined(choice))
         {
-            HighlightConsoleLine("[ERROR]: Invalid choice. Please enter a number corresponding to an option.", ConsoleColor.Red);
+            HighlightConsoleLine("[ERROR]: Invalid input. Please try again.", ConsoleColor.Red);
         }
 
-        return (T)(object)choice;
+        return choice;
     }
 
-    public static T GetEnumChoice<T>(string messageCategory = "") where T : struct, Enum
-    {
-        T[] values = Enum.GetValues<T>();
-
-        return GetEnumChoice(values, messageCategory);
-    }
+    public static T GetEnumChoice<T>(string messageCategory = "") where T : struct, Enum => GetEnumChoice(Enum.GetValues<T>(), messageCategory);
 }
