0c18efa9acb864bb096d4e280f28f56640cde6b6
OverwrittenCode <kylethapar2007@outlook.com>
2025-01-11 22:26:09 +0000

    refactor (Edexcel BTEC): redo EmployeeSalary

--- Edexcel BTEC/EmployeeSalary/Program.cs
+++ Edexcel BTEC/EmployeeSalary/Program.cs
@@ -1,26 +1,48 @@
-decimal salary = 0;
+using System.Globalization;
+
+CultureInfo.CurrentCulture = new("en-GB");
+
+var salary = 0m;
 
 for (var week = 1; week <= 4; week++)
 {
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"[WEEK {week}/4]");
+    Console.ResetColor();
+
     int hours;
-
-    Console.WriteLine($"[Week {week + 1}/4]: How many hours has the employee worked? (0 - 100)");
-
-    do
+    while (true)
     {
-        Console.Write("> ");
-    } while (!Int32.TryParse(Console.ReadLine(), out hours) || hours is < 0 or > 100);
+        const int MaxHours = 100;
+        Console.Write($"How many hours has the employee worked (up to {MaxHours})?: ");
 
-    var rate = hours > 40 ? 22.50M : 15.00M;
+        var input = Console.ReadLine();
+        Console.WriteLine();
+
+        if (Int32.TryParse(input, out hours) && hours is > 0 and <= MaxHours)
+        {
+            break;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid input. Try again.");
+        Console.ResetColor();
+    }
+
+    var rate = hours > 40 ? 22.50m : 15.00m;
 
     salary += hours * rate;
 
     if (hours > 45)
     {
-        salary += 100M;
+        salary += 100m;
     }
 }
 
-salary *= 0.9M;
+var tax = salary * 0.10m;
+salary -= tax;
 
-Console.WriteLine($"Salary for the month: {salary:C}");
+Console.Write("Salary for the month: ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"{salary:C}");
+Console.ResetColor();
